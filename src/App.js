75105714fbd990c71a69cc6566e60af88985c162
Bastien Cordier <bastien.cordier78@gmail.com>
2021-12-18 17:30:04 +0100

    Init archi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import Pokemons from "./containers/Pokemons";
+
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="App">
+          <h1>Hello</h1>
+          <Pokemons />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/Pokemons.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Pokemons = () => {
+  return (
+    <Fragment>
+      <p>Liste des pokémons</p>
+    </Fragment>
+  );
+};
+
+export default Pokemons;
