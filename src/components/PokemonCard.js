18f6eca33de33608c2c79a6950c3510e14d96e96
Bastien Cordier <bastien.cordier78@gmail.com>
2021-12-20 20:10:26 +0100

    Import images & infos

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,1 +1,28 @@
+import React from "react";
+import { Badge, Button, Card } from "react-bootstrap";
 
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <Card className="my-3" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={pokemon.sprites.other.home.front_default} />
+      <Card.Body>
+        <Card.Title>{pokemon.name}</Card.Title>
+        <Card.Text>
+          <b>Height :</b> {pokemon.height}
+        </Card.Text>
+        <Card.Text>Abilities :</Card.Text>
+        <div className="abilities">
+          {pokemon.types.map((abilities, idx) => {
+            return (
+              <div key={idx} className="mx-1">
+                <Badge bg="primary">{abilities.type.name}</Badge>
+              </div>
+            );
+          })}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default PokemonCard;
--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,32 +1,40 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, useEffect } from "react";
+import PokemonCard from "./PokemonCard";
 import axios from "axios";
 
-export default class PokemonsList extends Component {
-  state = {
-    pokemons: [],
-  };
+export default function PokemonsList() {
+  const [pokemons, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  async componentDidMount() {
-    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
-    this.setState({ pokemons: res.data["results"] });
-  }
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        return res.data.results;
+      })
+      .then((results) => {
+        return Promise.all(results.map((res) => axios.get(res.url)));
+      })
+      .then((results) => {
+        setLoading(false);
+        setPokemon(results.map((res) => res.data));
+      });
+  }, []);
 
-  render() {
-    return (
-      <Fragment>
-        {this.state.pokemons ? (
-          <div className="pokemons">
-            {this.state.pokemons.map((pokemon) => (
-              <>
-                <h4>{pokemon.name}</h4>
-                <img src={pokemon.sprites} alt="hello" />
-              </>
-            ))}
-          </div>
-        ) : (
-          <h2>Load</h2>
-        )}
-      </Fragment>
-    );
-  }
+  return (
+    <div>
+      {loading ? (
+        <h2>Loading ...</h2>
+      ) : (
+        <div className="pokemons">
+          {pokemons.map((pokemon, i) => (
+            <div key={i}>
+              <PokemonCard pokemon={pokemon} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
